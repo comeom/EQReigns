bbbcd6e257e0c7f1ecb6b86fc102ad1b86281b98
Comeom Rice <cch9399@stern.nyu.edu>
2021-10-15 15:06:43 -0400

    Initial Commit

---
+++ EQReigns/EQReignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EQReignsApp.swift
+//  EQReigns
+//
+//  Created by Wayne on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct EQReignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
